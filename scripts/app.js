014021312baad21a0d606b7b6d51120818bd284f
Cole0917 <colejb17@gmail.com>
2023-02-12 18:44:44 -0500

    finished services page and fixed some typos

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,7 @@ Date: 2023-02-12
 
         // bottom navbar (implemented for every other display function as well)
         
-
+        // this is to change the html text for 'Products' in the navbar
         document.getElementById("projects").innerHTML = "Projects";
         
         // another way to access body
@@ -56,7 +56,7 @@ Date: 2023-02-12
         secondaryParagraph.setAttribute("class", "mt-3 container")
 
         let quaternaryParagraph = document.createElement("h5")
-        secondaryParagraph.setAttribute("id", "TertiaryParagraph")
+        secondaryParagraph.setAttribute("id", "QuaternaryParagraph")
         secondaryParagraph.setAttribute("class", "mt-3 container")
 
  
@@ -79,11 +79,79 @@ Date: 2023-02-12
     }
 
     function DisplayServices() {
+        let mainContent = document.getElementsByTagName("main")[0]
+        mainContent.setAttribute("class", "container")
+        
         document.getElementById("projects").innerHTML = "Projects";
+        
+        // another way to access body
+        documentBody = document.body
+
+
+        let mainParagraph = document.createElement("h4")
+        mainParagraph.setAttribute("id", "MainParagraph")
+        mainParagraph.setAttribute("class", "mt-3 container")
+
+        let secondaryParagraph = document.createElement("h5")
+        secondaryParagraph.setAttribute("id", "SecondaryParagraph")
+        secondaryParagraph.setAttribute("class", "mt-3 container")
+
+        let tertiaryParagraph = document.createElement("h5")
+        secondaryParagraph.setAttribute("id", "TertiaryParagraph")
+        secondaryParagraph.setAttribute("class", "mt-3 container")
+
+        let quaternaryParagraph = document.createElement("h5")
+        secondaryParagraph.setAttribute("id", "QuaternaryParagraph")
+        secondaryParagraph.setAttribute("class", "mt-3 container")
+
+ 
+        let firstString = "Three Skills we offer to our clients: "
+        let secondString = "1. Web Development"
+        let thirdString = "2. Front End Design"
+        let fourthString = "3. Back End Programming"
+
+        mainParagraph.textContent = firstString
+        secondaryParagraph.textContent = secondString
+        tertiaryParagraph.textContent = thirdString
+        quaternaryParagraph.textContent = fourthString
+        
+
+        // add after(append)
+        mainContent.appendChild(mainParagraph)
+        mainContent.appendChild(secondaryParagraph)
+        mainContent.appendChild(tertiaryParagraph)
+        mainContent.appendChild(quaternaryParagraph)
     }
 
     function DisplayAboutUs() {
+        let mainContent = document.getElementsByTagName("main")[0]
+        mainContent.setAttribute("class", "container")
+        
         document.getElementById("projects").innerHTML = "Projects";
+        
+        // another way to access body
+        documentBody = document.body
+
+        
+
+        let mainParagraph = document.createElement("h4")
+        mainParagraph.setAttribute("id", "MainParagraph")
+        mainParagraph.setAttribute("class", "mt-3 container")
+
+        let secondaryParagraph = document.createElement("h4")
+        secondaryParagraph.setAttribute("id", "SecondaryParagraph")
+        secondaryParagraph.setAttribute("class", "mt-3 container")
+
+        let firstString = "Cole Biglang-awa (Computer Programming and Analysis: Second Year)"
+        let secondString = "Nathan Mcquaid (Computer Programming and Analysis: Second Year)"
+
+        mainParagraph.textContent = firstString
+        secondaryParagraph.textContent = secondString
+        
+
+        // add after(append)
+        mainContent.appendChild(mainParagraph)
+        mainContent.appendChild(secondaryParagraph)
     }
 
     function DisplayContactUs() {
